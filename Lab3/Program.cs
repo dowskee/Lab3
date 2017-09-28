c2c348ba9e98e79dd6f9d344e64c61ba9f37f941
dowskee <31831031+dowskee@users.noreply.github.com>
2017-09-28 09:25:56 -0400

    Added comments and new prompt challenge

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -14,19 +14,19 @@ namespace Lab3
             bool Continue = true;
             while (Continue == true)
             {
-                int UserInput;
+                int UserInput; 
                 Console.WriteLine("Learn your squares and cubes!");
                 Console.WriteLine("Enter an integer:");
                 UserInput = int.Parse(Console.ReadLine());
-                Console.WriteLine("Number" + "\t" + "Squared" + "\t" + "Cubed");
+                Console.WriteLine("Number" + "\t" + "Squared" + "\t" + "Cubed"); //challenge: use pad instead of tab?
 
                 for (int i = 1; i <= UserInput; i++) 
 
                 {
-                    Console.WriteLine(i + "\t" + Math.Pow(i, 2) + "\t" + Math.Pow(i, 3));
+                    Console.WriteLine(i + "\t" + Math.Pow(i, 2) + "\t" + Math.Pow(i, 3)); //number, tab, squared, tab, cubed.
                 }  
 
-                string UserChoice;
+                string UserChoice; //loop back
                 Console.WriteLine("Continue? (y/n?)");
                 UserChoice = Console.ReadLine();
                 if (UserChoice == "N" || UserChoice == "No"|| UserChoice == "no" || UserChoice == "n")
